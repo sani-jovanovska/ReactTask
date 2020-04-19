5a78639e24b4e52071659e04a6b8620437fd2e4e
sanijovanovska <sanijovanovska@gmail.com>
2020-04-19 21:41:03 +0200

    promena vo objektite

--- src/recepies/recepies.js
+++ src/recepies/recepies.js
@@ -4,36 +4,36 @@ export default function() {
             id:0,
             name: "Cake",
             source: "https://img.taste.com.au/-RGbsS2h/taste/2019/05/chocolate-and-nutella-smores-cake-149475-2.jpg",
-            ingredients:["Mleko","Seker","Brasno","Sol","Kvasec"],
-            time: 30,
-            instructions:"Mix together flour, cocoa powder, baking powder and baking soda. In a large bowl, beat butter, eggs and vanilla. Gradually add sugar. Beat on medium to high speed for about 3-4 minutes until well mixed."       
+            ingredients:["175g (6oz) margarine or softened butter","175g (6oz) caster sugar","3 large eggs","175g (6oz) self-raising flour, sifted","1tsp baking powder","1tsp vanilla extract","pinch of salt"],
+            time: 60,
+            instructions:"Heat the oven to 180°C (gas mark 4). Lightly grease an 18cm (7in) round cake tin with a little extra butter or margarine and cut a piece of greaseproof paper or non-stick baking parchment to fit the base of the tin.Put all the ingredients into a large mixing bowl and beat with a wooden spoon or a hand-held mixer for 1 minute, or until just combined. It's important not to beat the batter too much - just long enough to make it smooth.Pour or spoon the mixture into the tin, smooth the top and bake on the middle shelf of the oven for about 45-50 minutes. The cake is cooked when it looks well risen and golden; the top should spring back when lightly touched with a fingertip. Another test is to insert a skewer into the centre of the cake - it should come out clean.Let the cake sit in the tin for 5 minutes, then gently run a knife around the edge and turn the cake out onto a wire rack to cool. Serve dusted with icing sugar."       
 
         },
         {
             id:1,
             name: "Pudding",
             source: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190628-delish-chocolate-pudding-ehg-305-horizontal-1562792024.png",
-            ingredients:["Mleko","Seker","Brasno"],
+            ingredients:["3 cups of milk","1 cup of sugar","Pudding powder"],
             time: 30,
-            instructions:"Mix together flour, cocoa powder, baking powder and baking soda. In a large bowl, beat butter, eggs and vanilla. Gradually add sugar. Beat on medium to high speed for about 3-4 minutes until well mixed."  
+            instructions:"Heat the milk, then mix together the pudding powder and the sugar. Put them in the heated milk and stir. "  
 
         },
         {
             id:2,
             name: "Apple pie",
             source: "https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/17607-caramel-apple-pie-760x580.jpg?ext=.jpg",
-            ingredients:["Mleko","Seker","Brasno"],
-            time: 30,
-            instructions:"dshbwfsnfcjenrcfjkrw"  
+            ingredients:["1 recipe pastry for a 9 inch double crust pie","½ cup unsalted butter","3 tablespoons all-purpose flour", "¼ cup water","½ cup white sugar","½ cup packed brown sugar","8 Granny Smith apples - peeled, cored and sliced"],
+            time: 60,
+            instructions:"Preheat oven to 425 degrees F (220 degrees C). Melt the butter in a saucepan. Stir in flour to form a paste. Add water, white sugar and brown sugar, and bring to a boil. Reduce temperature and let simmer.Place the bottom crust in your pan. Fill with apples, mounded slightly. Cover with a lattice work crust. Gently pour the sugar and butter liquid over the crust. Pour slowly so that it does not run off.Bake 15 minutes in the preheated oven. Reduce the temperature to 350 degrees F (175 degrees C). Continue baking for 35 to 45 minutes, until apples are soft."  
 
         },
         {
             id:3,
             name: "Pancakes",
             source: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190403-pancakes-066-copy-1554497284.jpeg?crop=0.777xw:0.584xh;0.115xw,0.315xh&resize=1200:*",
-            ingredients:["Mleko","Seker","Brasno","Zejtin","Sani"],
+            ingredients:["2 large eggs","2 cups 250ml milk (skim, low fat or full fat)","2 1/4 cups white self raising flour*"],
             time: 30,
-            instructions:"dshbwfsnfcjenrcfjkrw"  
+            instructions:"Whisk the eggs and milk together to combine. Add in the flour, and beat until the batter is smooth and lump free. Add in any option add-ins you like here.Heat a nonstick pan over low-medium heat with cooking oil spray. Wipe over excess; pour about 1/3 cup of batter per pancake. Cook pancakes for 1 to 2 minutes, or until bubbles appear on the surface and the bases are golden brown. Flip and cook until golden; transfer to a warmed plate; repeat with remaining batter (spraying/greasing pan between ever second or third pancake if needed)."  
 
         }
     ]
